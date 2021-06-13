eb5974052f779dfb4dfd89d0faec4c0b1d715ded
Ezzud <44119886+Ezzud@users.noreply.github.com>
2021-06-13 23:57:24 +0200

    Some Improvements
    <p>
    
    Added duration with **%duration%** variable in "track" value in `config.json`
    %duration% will display a progress bar with the time played and the total time of the current track
    </p>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
 
 
-	File and package requirement
+    File and package requirement
 
 
 */
@@ -11,11 +11,10 @@ const FileCookieStore = require('tough-cookie-filestore2');
 const credentials = require('./credentials.json');
 const cookieStore = new FileCookieStore('./cookies.json');
 const config = require('./config.json');
-
 /*
 
 
-	Variables
+    Variables
 
 
 */
@@ -23,11 +22,10 @@ let spotifyReady = false;
 let instaReady = false;
 var insta_acc;
 var spotify_acc;
-
 /*
 
 
-	Check if missing credentials
+    Check if missing credentials
 
 
 */
@@ -39,12 +37,10 @@ if (!credentials.spotifyClientID || !credentials.spotifyClientSecret || !credent
     console.error(`Une valeur de connexion est manquante dans le fichier "credentials.json"`);
     process.exit(0);
 }
-
-
 /*
 
 
-	API Calls
+    API Calls
 
 
 */
@@ -61,13 +57,10 @@ const client = new Instagram({
     password: password,
     cookieStore: cookieStore
 });
-
-
-
 /*
 
 
-	Account connection test
+    Account connection test
 
 
 */
@@ -77,41 +70,35 @@ async function spotifyConnect() {
     if (!profile) {
         return false;
     } else {
-    	spotify_acc = profile.body.display_name
+        spotify_acc = profile.body.display_name
         spotifyReady = true
         return true;
     }
 }
-
 async function instagramConnect() {
-client.login()
-  	  .then(async() => {
-  	  	instaReady = true
-    	let profile = await client.getProfile()
-    	console.log(`Connected as ${profile.username}`)
-    	insta_acc = profile.username
- 	   })
-  	  .catch(err => {
-  	  	console.error(err)
-  	  })
+    client.login().then(async () => {
+        instaReady = true
+        let profile = await client.getProfile()
+        console.log(`Connected as ${profile.username}`)
+        insta_acc = profile.username
+    }).catch(err => {
+        console.error(err)
+    })
 }
-
-
-
 /*
 
 
-	Main function
+    Main function
 
 
 */
 async function run() {
-	let oldtitle;
-	await instagramConnect()
-	await refresh()
+    let oldtitle;
+    await instagramConnect()
+    await refresh()
     setInterval(async () => {
         if (await spotifyConnect() === false) return;
-        if(instaReady === false) return;
+        if (instaReady === false) return;
         let data = await spotifyApi.getMyCurrentPlaybackState().catch(err => {
             console.error(err)
         })
@@ -120,12 +107,30 @@ async function run() {
                 console.error(err)
             })
             let title = song.body.item.name
-            if(title === oldtitle) {
+            if (title === oldtitle) {
+            let artist = song.body.item.artists[0].name;
+            let timefor = await formatDate(song.body.progress_ms);
+            let timeto = await formatDate(song.body.item.duration_ms);
+            let pourcent = (song.body.progress_ms * 100) / song.body.item.duration_ms
+            pourcent = await makePourcent(pourcent, timefor, timeto)
+            let format = config.track.replace("%title%", title).replace("%author%", artist).replace("%duration%", pourcent);
+            format = config.bio.replace("%track%", format)
+            if (instaReady === false) return;
+            let profile = await client.getProfile()
+            await client.updateProfile({
+                username: profile.username,
+                name: profile.first_name,
+                gender: profile.gender,
+                phoneNumber: profile.phone_number,
+                email: profile.email,
+                website: profile.external_url,
+                biography: `${format}`
+            })
             let bio = await client.getProfile()
             bio = bio.biography
-            console.clear()
-            console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}`)
-            return;
+                console.clear()
+                console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}`)
+                return;
             }
             oldtitle = title;
             let artist = song.body.item.artists[0].name;
@@ -135,76 +140,87 @@ async function run() {
             pourcent = await makePourcent(pourcent, timefor, timeto)
             let format = config.track.replace("%title%", title).replace("%author%", artist).replace("%duration%", pourcent);
             format = config.bio.replace("%track%", format)
-    		if(instaReady === false) return;
+            if (instaReady === false) return;
             let profile = await client.getProfile()
-            await client.updateProfile({ username:profile.username, name:profile.first_name, gender:profile.gender, phoneNumber:profile.phone_number, email:profile.email, website:profile.external_url, biography:`${format}`})
+            await client.updateProfile({
+                username: profile.username,
+                name: profile.first_name,
+                gender: profile.gender,
+                phoneNumber: profile.phone_number,
+                email: profile.email,
+                website: profile.external_url,
+                biography: `${format}`
+            })
             let bio = await client.getProfile()
             bio = bio.biography
             console.clear()
             console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}\nPourcent: ${pourcent}`)
             console.log("Bio updated!")
         } else {
-        	let title = config.nolistening
-        	if(title === oldtitle) {
-                    let bio = await client.getProfile()
-            bio = bio.biography
-            console.clear()
-            console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}`)
-            return;
-        	}
-        	oldtitle = title
-        	let format = config.bio.replace("%track%", config.nolistening)
-        	let profile = await client.getProfile()
-            await client.updateProfile({ username:profile.username, name:profile.first_name, gender:profile.gender, phoneNumber:profile.phone_number, email:profile.email, website:profile.external_url, biography:`${format}`})
+            let title = config.nolistening
+            if (title === oldtitle) {
+                let bio = await client.getProfile()
+                bio = bio.biography
+                console.clear()
+                console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}`)
+                return;
+            }
+            oldtitle = title
+            let format = config.bio.replace("%track%", config.nolistening)
+            let profile = await client.getProfile()
+            await client.updateProfile({
+                username: profile.username,
+                name: profile.first_name,
+                gender: profile.gender,
+                phoneNumber: profile.phone_number,
+                email: profile.email,
+                website: profile.external_url,
+                biography: `${format}`
+            })
             let bio = await client.getProfile()
             bio = bio.biography
             console.clear()
             console.log(`Compte Spotify: ${spotify_acc || `Inconnu`}\nCompte Instagram: ${insta_acc || `Inconnu`}\nBio: ${bio}`)
             console.log("Bio updated!")
         }
-    }, 5000);
+    }, 10000);
 }
 
 function refresh() {
-	    spotifyApi.refreshAccessToken().then(function(data) {
-        	console.log('The access token has been refreshed!');
-        	spotifyApi.setAccessToken(data.body['access_token']);
-    	}, function(err) {
-        	console.log('Could not refresh access token', err);
-    	});
-	setInterval(async () => {
-	    spotifyApi.refreshAccessToken().then(function(data) {
-        	console.log('The access token has been refreshed!');
-        	spotifyApi.setAccessToken(data.body['access_token']);
-    	}, function(err) {
-        	console.log('Could not refresh access token', err);
-    	});
-	}, 60000)
+    spotifyApi.refreshAccessToken().then(function(data) {
+        console.log('The access token has been refreshed!');
+        spotifyApi.setAccessToken(data.body['access_token']);
+    }, function(err) {
+        console.log('Could not refresh access token', err);
+    });
+    setInterval(async () => {
+        spotifyApi.refreshAccessToken().then(function(data) {
+            console.log('The access token has been refreshed!');
+            spotifyApi.setAccessToken(data.body['access_token']);
+        }, function(err) {
+            console.log('Could not refresh access token', err);
+        });
+    }, 60000)
 }
-
 
 function formatDate(t) {
     let remainingTime = new Date(t)
     let roundTowardsZero = remainingTime > 0 ? Math.floor : Math.ceil;
     let minutes = roundTowardsZero(remainingTime / 60000) % 60,
-    seconds = roundTowardsZero(remainingTime / 1000) % 60;
+        seconds = roundTowardsZero(remainingTime / 1000) % 60;
     let m;
     let s;
-    if(minutes > 0) {
-       m = `${minutes}m` 
-    } else if(minutes === 0) {
-       m = ``
-    } else if(minutes === 1){
+    if (minutes > 0) {
         m = `${minutes}m`
+    } else if (minutes === 0) {
+        m = ``
     } else {
         m = ``
     }
-    if(seconds > 0) {
-       s = `${seconds}s` 
-    } else if(seconds === 0) {
-       s = ``
-    } else if(seconds === 1) {
+    if (seconds > 0) {
         s = `${seconds}s`
+    } else if (seconds === 0) {
+        s = ``
     } else {
         s = ``
     }
@@ -214,15 +230,15 @@ function formatDate(t) {
 
 function makePourcent(n, tf, tt) {
     let pr = "0s [—————] 0s";
-    if(n <= 20) {
+    if (n <= 20) {
         pr = `${tf} [🟠————] ${tt}`
-    } else if(n > 20 && n <= 40) {
+    } else if (n > 20 && n <= 40) {
         pr = `${tf} [—🟠———] ${tt}`
-    } else if(n > 40 && n <= 60) {
+    } else if (n > 40 && n <= 60) {
         pr = `${tf} [——🟠——] ${tt}`
-    } else if(n > 60 && n <= 80) {
+    } else if (n > 60 && n <= 80) {
         pr = `${tf} [———🟠—] ${tt}`
-    } else if(n > 80 && n <= 100) {
+    } else if (n > 80 && n <= 100) {
         pr = `${tf} [————🟠] ${tt}`
     }
     return pr;
